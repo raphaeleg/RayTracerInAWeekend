9b7d8761dde37d35dddd7ca83c16c623f4e222af
raphaeleg <75595961+raphaeleg@users.noreply.github.com>
2024-06-08 17:01:54 +0800

    5. Adding a Sphere
    Added `hit_sphere()` to check whether a ray has hit a sphere

--- RayTracerInAWeekend/main.cpp
+++ RayTracerInAWeekend/main.cpp
@@ -36,7 +36,19 @@ struct Camera {
 	}
 };
 
-static color ray_color(const ray& r) {
+static bool hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto b = -2.0 * dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
+color ray_color(const ray& r) {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+		return color(1, 0, 0);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y + 1.0);
 	return (1.0 - a) * vec3(1.0f) + a * vec3(0.5f, 0.7f, 1.0f);
@@ -61,5 +73,5 @@ int main() {
 			write_color(std::cout, pixel_color);
 		}
 	}
-	//std::clog << "\rDone.                      \n";
+	std::clog << "\rDone.                      \n";
 }
